f00beff3abb435d5a6c4c94e43b00f905c6db510
PrinceSandal20 <84025662+PrinceSandal20@users.noreply.github.com>
2022-03-04 17:16:36 +0530

    Add files via upload

---
+++ user/models.py
@@ -0,0 +1,23 @@
+from email.policy import default
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+from sqlalchemy import null, true
+
+class Profile(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+	image = models.ImageField(default='profile/profile_pics/default.jpg',upload_to='profile/profile_pics')
+
+	def __str__(self):
+		return f'{self.user.username} Profile'
+
+
+	def save(self,**kwargs):
+		super().save()
+
+		img = Image.open(self.image.path)
+
+		if img.height > 300	 or img.width > 300:
+			output_size = (300,300)
+			img.thumbnail(output_size)
+			img.save(self.image.path)	
